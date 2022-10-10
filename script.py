d898a92290b99eb001c72efdca879fd6185062fe
Leslie Pagaduan <114967698+EL1234567@users.noreply.github.com>
2022-10-10 02:23:18 -0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def message():
+    return render_template('message.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Priv
